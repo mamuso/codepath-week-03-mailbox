47fcdcdf34d509e76aa8d9ecc783a6f0aaf6ffa3
Manuel Muñoz Solera <mamuso@mamuso.net>
2015-09-29 19:07:43 -0700

    Initial Submission

---
+++ codepath-week-03-mailboxTests/codepath_week_03_mailboxTests.swift
@@ -0,0 +1,36 @@
+//
+//  codepath_week_03_mailboxTests.swift
+//  codepath-week-03-mailboxTests
+//
+//  Created by mmuno on 9/29/15.
+//  Copyright © 2015 mamuso. All rights reserved.
+//
+
+import XCTest
+@testable import codepath_week_03_mailbox
+
+class codepath_week_03_mailboxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
